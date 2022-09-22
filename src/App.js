3077c326d21e7f8c90f12b0fa13adf184ad80bf8
kiruthiga19 <kiruthigapadmavathi@gmail.com>
2022-09-22 19:10:54 +0530

    git render method

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import ClickCounter from './component/ClickCounter';
+import HoverCounter from './component/HoverCounter';
+import User from './component/User';
+import Counter from './component/Counter';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    {/* <ClickCounter></ClickCounter>
+    <HoverCounter></HoverCounter>
+    <User render={ (isLoggedIn)=> isLoggedIn?'kiru':'guest'}></User> */}
+    <Counter render={(count,incrementCount)=><ClickCounter count={count} 
+    incrementCount={incrementCount}/>}></Counter>
+     <Counter render={(count,incrementCount)=><HoverCounter count={count} 
+    incrementCount={incrementCount}/>}></Counter>
     </div>
   );
 }
