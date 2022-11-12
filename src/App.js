548ad6760e6c3360a0671a7f89c575ed91f9db59
kiruthiga19 <kiruthigapadmavathi@gmail.com>
2022-11-12 11:21:25 +0530

    react-reducx

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import { Provider } from 'react-redux';
+import store from './redux/store';
+import CakeContainer from './Component/CakeContainer';
+import HooksCakeContainer from './Component/HooksCakeContainer';
 
 function App() {
   return (
+    <Provider store={store}>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <CakeContainer></CakeContainer>
+     <HooksCakeContainer></HooksCakeContainer>
     </div>
+    </Provider>
   );
 }
 
